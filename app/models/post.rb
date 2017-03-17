0dfe306e30b90b9528d4672596d2a4d51679a0d9
hitesh kumar mahour <hkhiteshm@gmail.com>
2017-03-17 21:12:00 +0530

    changed missing.png default link

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
      medium: "500x500>",
      large: "1000x1000>",
      thumb: "100x100>" },
-     default_url: "/assets/images/:style/missing.png"
+     default_url: "https://s3.amazonaws.com/instatestbuck/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :description, presence: true, length:{minimum:3};
 end
