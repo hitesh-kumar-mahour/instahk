ffe14eebe0b858ab0b2e105e86557d5f65685f60
hitesh kumar mahour <hkhiteshm@gmail.com>
2017-03-19 04:22:46 +0530

    corrected form for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,4 @@
 class CommentsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
 
   def destroy
     @post=Post.find(params[:post_id])
@@ -12,22 +7,11 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def new
-  end
-
   def create
       @post= Post.find( params[:post_id] )
       @comment= @post.comments.create(params[:comment].permit(:name,:body))
       redirect_to post_path(@post)
   end
-
-  def edit
-    end
-
-  def update
-
-  end
-
   # private
   #
   #   def permit_comment
